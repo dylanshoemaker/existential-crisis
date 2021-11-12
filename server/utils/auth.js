94116ea6921a212fee09379b2fc843354c18f625
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-12 13:03:05 -0800

    implement auth middleware tokens to populate me query

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,39 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 dotenv.config();
-
 
 const secret = process.env.AUTH_SECRET;
 const expiration = process.env.AUTH_EXPIRATION;
 
 module.exports = {
-  signToken: function({ username, email, _id }) {
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // separate "Bearer" from "<tokenvalue>"
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    // if no token, return request object as is
+    if (!token) {
+      return req;
+    }
+
+    try {
+      // decode and attach user data to request object
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    // return updated request object
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  }
+  },
 };
