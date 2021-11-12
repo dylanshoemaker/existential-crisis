c3a1c2f03460db6de10085c4150eb6613710e0a8
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-12 13:43:41 -0800

    fix the me user fineone bug and make schema code more readable by restructuring it

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,12 @@
-const { User, Thought } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
+const { User, Thought } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({})
+        const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
           .populate("thoughts")
           .populate("friends");
@@ -43,6 +43,22 @@ const resolvers = {
       const user = await User.create(args);
       const token = signToken(user);
 
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
       return { token, user };
     },
     addThought: async (parent, args, context) => {
@@ -86,22 +102,7 @@ const resolvers = {
     
       throw new AuthenticationError('You need to be logged in!');
     },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
-
-      const token = signToken(user);
-      return { token, user };
-    },
+    
   },
 };
 
