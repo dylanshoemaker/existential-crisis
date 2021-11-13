958e1b80b6f73f6dac43570dab32e46087284b60
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-13 14:02:08 -0800

    create ThoughtForm component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,17 +3,24 @@ import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
 import ThoughtList from "../components/ThoughtList";
 import Auth from "../utils/auth";
-import FriendList from '../components/FriendList';
+import FriendList from "../components/FriendList";
+import ThoughtForm from "../components/ThoughtForm";
+
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const { data: userData } = useQuery(QUERY_ME_BASIC);
   const thoughts = data?.thoughts || [];
   const loggedIn = Auth.loggedIn();
-  
+
   return (
     <main>
       <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {loading ? (
             <div>Loading...</div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,7 @@ import FriendList from "../components/FriendList";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -43,6 +44,7 @@ const Profile = () => {
   return (
     <div>
       <div className="flex-row mb-3">
+
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
@@ -52,7 +54,7 @@ const Profile = () => {
           </button>
         )}
       </div>
-
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList
