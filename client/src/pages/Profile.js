293fb61b3f4096ee5bcad3187e662fb40e128da1
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-13 13:51:40 -0800

    add a Add Friend button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Redirect, useParams } from "react-router-dom";
-
+import { ADD_FRIEND } from "../utils/mutations";
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
 
 const Profile = () => {
   const { username: userParam } = useParams();
-
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
-
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
   const user = data?.me || data?.user || {};
   //this redirects the user if they look up their own url so rather than /profile/username, they would be routed to /profile, its like using location.replace() but with React
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -23,11 +31,12 @@ const Profile = () => {
   if (loading) {
     return <div>Loading...</div>;
   }
-  
+
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
       </h4>
     );
   }
@@ -37,6 +46,11 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
